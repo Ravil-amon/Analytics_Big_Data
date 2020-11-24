88a1e535a88392938082c62968b548b3b45da291
Ravil-amon <74340160+Ravil-amon@users.noreply.github.com>
2020-11-24 13:58:33 +0500

    Delete time_2.py

--- time_2.py
+++
@@ -1,6 +0,0 @@
-# Ввод времени в секундах. Вывод в чч:мм:сс.
-seconds = int(input('Введите время в секундах: '))
-hour = seconds // 3600
-minute = (seconds % 3600) // 60
-second = (seconds % 3600) % 60
-print(f'{hour}:{minute}:{second}')
